f68b0a3d3fd77f6e6be3a04f82d275ae53df6741
Antor4q <tariquelislam2015@gmail.com>
2024-03-28 15:39:05 +0600

    website deploy finished on netlify

--- src/pages/PagesRead/PagesRead.jsx
+++ src/pages/PagesRead/PagesRead.jsx
@@ -1,71 +1,34 @@
 // import React, { useEffect, useState } from 'react';
 // import { useLoaderData } from 'react-router-dom';
-import { BarChart,  XAxis, YAxis, CartesianGrid, Bar, Cell, ResponsiveContainer } from 'recharts';
+import { BarChart,  XAxis, YAxis, CartesianGrid, Bar, Cell, ResponsiveContainer, Tooltip } from 'recharts';
+import { getStoredBooks } from '../../components/utility/localStorage';
+import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 // import { getStoredId } from '../../components/utility/localStorage';
 
 const PagesRead = () => {
 
-//     const books = useLoaderData()
-//     const [storedBooks,setStoredBooks] = useState([])
+    const books = useLoaderData()
+    const [storedBooks,setStoredBooks] = useState([])
    
-//     useEffect(()=>{
-//         const savedBooksId = getStoredId()
+    useEffect(()=>{
+        const savedBooksId = getStoredBooks()
        
-//        if(books.length > 0){
-//          const bookAdded = books.filter(book => savedBooksId.includes(book.bookId))
-//          setStoredBooks(bookAdded)
+       if(books.length > 0){
+         const bookAdded = books.filter(book => savedBooksId.includes(book.bookId))
+         setStoredBooks(bookAdded)
+         console.log(bookAdded)
+
          
-//          }
-//      },[books])
+         }
+         
+     },[books])
+  
      
-//  console.log(storedBooks)
-    
+   
 
     const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink']
-    const data = [
-        {
-          name: 'Page A',
-          uv: 4000,
-          pv: 2400,
-          amt: 2400,
-        },
-        {
-          name: 'Page B',
-          uv: 3000,
-          pv: 1398,
-          amt: 2210,
-        },
-        {
-          name: 'Page C',
-          uv: 2000,
-          pv: 9800,
-          amt: 2290,
-        },
-        {
-          name: 'Page D',
-          uv: 2780,
-          pv: 3908,
-          amt: 2000,
-        },
-        {
-          name: 'Page E',
-          uv: 1890,
-          pv: 4800,
-          amt: 2181,
-        },
-        {
-          name: 'Page F',
-          uv: 2390,
-          pv: 3800,
-          amt: 2500,
-        },
-        {
-          name: 'Page G',
-          uv: 3490,
-          pv: 4300,
-          amt: 2100,
-        },
-      ];
+    const data = storedBooks
       const getPath = (x, y, width, height) => {
         return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
         ${x + width / 2}, ${y}
@@ -97,9 +60,10 @@ const PagesRead = () => {
             >
 
         <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
+      <XAxis dataKey={"bookName"} />
       <YAxis />
-      <Bar dataKey="uv" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+      <Tooltip></Tooltip>
+      <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
         {data.map((entry, index) => (
           <Cell key={`cell-${index}`} fill={colors[index % 20]} />
         ))}
